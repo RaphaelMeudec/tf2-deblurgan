abc2d8d7524b8d5b23d4fda66c3de5ff8578d8eb
Raphael Meudec <raphaelm@sicara.com>
2019-11-15 17:47:13 +0100

    Add files

---
+++ deblurgan/datasets.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+
+import cv2
+import tensorflow as tf
+
+
+def select_patch(sharp, blur, patch_size_x, patch_size_y):
+    stack = tf.stack([sharp, blur], axis=0)
+    patches = tf.image.random_crop(stack, size=[2, patch_size_x, patch_size_y, 3])
+    return (patches[0], patches[1])
+
+
+def get_dataset_path(dataset_name):
+    return Path("datasets") / dataset_name
+
+
+class IndependantDataLoader:
+    def image_dataset(self, images_paths):
+        dataset = tf.data.Dataset.from_tensor_slices(images_paths)
+        dataset = (
+            dataset.map(
+                tf.io.read_file, num_parallel_calls=tf.data.experimental.AUTOTUNE
+            )
+            .map(tf.image.decode_png, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+            .map(
+                lambda x: tf.image.convert_image_dtype(x, tf.float32),
+                num_parallel_calls=tf.data.experimental.AUTOTUNE,
+            )
+        )
+
+        return dataset
+
+    def load(
+        self,
+        dataset_name,
+        mode="train",
+        batch_size=4,
+        patch_size=(256, 256),
+        shuffle=False,
+    ):
+        dataset_path = get_dataset_path(dataset_name) / mode
+        sharp_images_path = [str(path) for path in dataset_path.glob("*/sharp/*.png")]
+        blur_images_path = [path.replace("sharp", "blur") for path in sharp_images_path]
+
+        sharp_dataset = self.image_dataset(sharp_images_path)  # .cache("sharp")
+        # Call dataset to populate cache
+        # for _ in sharp_dataset: pass
+        blur_dataset = self.image_dataset(blur_images_path)  # .cache("blur")
+        # Call dataset to populate cache
+        # for _ in blur_dataset: pass
+
+        dataset = tf.data.Dataset.zip((sharp_dataset, blur_dataset))
+
+        if shuffle:
+            dataset = dataset.shuffle(buffer_size=200)
+
+        dataset = dataset.map(
+            lambda sharp_image, blur_image: select_patch(
+                sharp_image, blur_image, patch_size[0], patch_size[1]
+            ),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        )
+
+        dataset = dataset.batch(batch_size)
+        dataset = dataset.repeat()
+        dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+        return dataset
+
+
+if __name__ == "__main__":
+    train_dataset = IndependantDataLoader().load(
+        "gopro", patch_size=(128, 128), batch_size=16, mode="train"
+    )
+    for sharps, blurs in train_dataset.take(1):
+        sample_sharp, sample_blur = sharps[0], blurs[0]
+
+        cv2.imwrite(
+            "sample_blur.png",
+            cv2.cvtColor(
+                (255 * sample_blur.numpy()).astype("uint8"), cv2.COLOR_RGB2BGR
+            ),
+        )
+        cv2.imwrite(
+            "sample_sharp.png",
+            cv2.cvtColor(
+                (255 * sample_sharp.numpy()).astype("uint8"), cv2.COLOR_RGB2BGR
+            ),
+        )
---
+++ deblurgan/losses.py
@@ -0,0 +1,35 @@
+import numpy as np
+import tensorflow.keras.backend as K
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.models import Model
+
+# Note the image_shape must be multiple of patch_shape
+image_shape = (256, 256, 3)
+
+
+def l1_loss(y_true, y_pred):
+    return K.mean(K.abs(y_pred - y_true))
+
+
+def perceptual_loss(y_true, y_pred, loss_factor=1):
+    vgg = VGG16(include_top=False, weights="imagenet", input_shape=image_shape)
+    loss_model = Model(inputs=vgg.input, outputs=vgg.get_layer("block3_conv3").output)
+    loss_model.trainable = False
+    return loss_factor * K.mean(K.square(loss_model(y_true) - loss_model(y_pred)))
+
+
+def wasserstein_loss(y_true, y_pred):
+    return K.mean(y_true * y_pred)
+
+
+def gradient_penalty_loss(y_true, y_pred, averaged_samples):
+    gradients = K.gradients(y_pred, averaged_samples)[0]
+    gradients_sqr = K.square(gradients)
+    gradients_sqr_sum = K.sum(
+        gradients_sqr, axis=np.arange(1, len(gradients_sqr.shape))
+    )
+
+    gradient_l2_norm = K.sqrt(gradients_sqr_sum)
+    gradient_penalty = K.square(1 - gradient_l2_norm)
+
+    return K.mean(gradient_penalty)
